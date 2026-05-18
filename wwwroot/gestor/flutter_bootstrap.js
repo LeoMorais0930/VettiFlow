0fa95fa4855fe191d9e628e6912a996a96197be2
Leonardo Morais <leonardohbrito57@gmail.com>
2026-05-18 11:38:12 -0300

    correcao bugs

--- wwwroot/gestor/flutter_bootstrap.js
+++ wwwroot/gestor/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2774675442"
+    serviceWorkerVersion: "185899574"
   }
 });
